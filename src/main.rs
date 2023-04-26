e8e029dc47297e5141b513a02e4412008c6b60cc
Folafunmi <folafunmimustapha@gmail.com>
2023-04-26 23:25:20 +0100

    feat: start server

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 
 use std::net::SocketAddr;
 
-use axum::response::Html;
+use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/", get(|| async { Html("Hello") }));
+    let routes_hello = Router::new().route("/", get(handler_home));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!(">>> Listening on {addr}\n");
@@ -16,4 +16,9 @@ async fn main() {
         .serve(routes_hello.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler_home() -> impl IntoResponse {
+    println!(">> {:<12} - handler_home", "HANDLER");
+    Html("Hello from home")
 }
